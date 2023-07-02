6245865f14bb3fe20c0719ae2f7a3bc5b320044b
haydar-abbas <haydarabbas40@proton.me>
2023-07-02 08:40:09 +0300

    Update Strategy Pattern

--- src/behavioural/strategy/Main.java
+++ src/behavioural/strategy/Main.java
@@ -6,15 +6,13 @@ import behavioural.strategy.flybehavior.FlyWithRocket;
 public class Main {
 
     public static void main(String[] args) {
-        Duck duck1 = new NormalDuck("Duck1");
-        System.out.println(duck1.fly());
-        duck1.setFlyBehavior(new FlyWithRocket());
-        System.out.println(duck1.fly());
+        Duck duck = new NormalDuck("Duck");
+        System.out.println(duck.fly());
 
-        Duck duck2 = new NormalDuck("duck2");
-        System.out.println(duck2.fly());
-        duck2.setFlyBehavior(new FlyNoWay());
-        System.out.println(duck2.fly());
+        duck.setFlyBehavior(new FlyWithRocket());
+        System.out.println(duck.fly());
 
+        duck.setFlyBehavior(new FlyNoWay());
+        System.out.println(duck.fly());
     }
 }
