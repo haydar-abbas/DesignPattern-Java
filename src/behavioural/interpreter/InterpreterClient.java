c789e14a3440ff8fcdf524b3807de142c0644e55
haydar-abbas <haydarabbas40@proton.me>
2023-08-22 15:30:47 +0300

    Update Interpreter Pattern

--- src/behavioural/interpreter/InterpreterClient.java
+++ src/behavioural/interpreter/InterpreterClient.java
@@ -2,12 +2,6 @@ package behavioural.interpreter;
 
 public class InterpreterClient {
 
-    public InterpreterContext ic;
-
-    public InterpreterClient(InterpreterContext ic) {
-        this.ic = ic;
-    }
-
     public String interpret(String str) {
         Expression exp;
         //create rules for expressions
@@ -16,14 +10,16 @@ public class InterpreterClient {
             exp = new IntToHexExpression(c);
         } else if (str.contains("Binary")) {
             exp = new IntToBinaryExpression(c);
-        } else return str;
-        return exp.interpret(ic);
+        } else {
+            return str;
+        }
+        return exp.interpret(new InterpreterContext());
     }
 
     public static void main(String[] args) {
         String str1 = "15 in Binary";
         String str2 = "15 in Hexadecimal";
-        InterpreterClient ec = new InterpreterClient(new InterpreterContext());
+        InterpreterClient ec = new InterpreterClient();
         System.out.println(str1 + "= " + ec.interpret(str1));
         System.out.println(str2 + "= " + ec.interpret(str2));
     }
