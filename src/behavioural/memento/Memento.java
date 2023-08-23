f9198d4355176179c009df82748ae39755ee12ea
haydar-abbas <haydarabbas40@proton.me>
2023-08-23 10:30:18 +0300

    Create Memento Pattern

---
+++ src/behavioural/memento/Memento.java
@@ -0,0 +1,14 @@
+package behavioural.memento;
+
+public class Memento {
+
+    private final String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
