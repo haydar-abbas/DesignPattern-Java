403d006c785edd101d30bba922f5d3507f503f04
heydar-abbas <haydarabbas40@proton.me>
2023-12-20 09:55:08 +0300

    Update Singleton Pattern

--- src/creational/singleton/Main.java
+++ src/creational/singleton/Main.java
@@ -3,21 +3,21 @@ package creational.singleton;
 public class Main {
 
     public static void main(String[] args) {
-//        Singleton obj1 = Singleton.getInstance();
-//        Singleton obj2 = Singleton.getInstance();
-//
-//        obj1.printData();
-//        obj2.printData();
+        Singleton obj1 = Singleton.getInstance();
+        Singleton obj2 = Singleton.getInstance();
 
-         new DataPrinter().start();
-         new DataPrinter().start();
+        obj1.printData();
+        obj2.printData();
+
+//         new DataPrinter().start();
+//         new DataPrinter().start();
 
     }
 }
 
-class DataPrinter extends Thread {
-    @Override
-    public void run() {
-        Singleton.getInstance().printData();
-    }
-}
+//class DataPrinter extends Thread {
+//    @Override
+//    public void run() {
+//        Singleton.getInstance().printData();
+//    }
+//}
